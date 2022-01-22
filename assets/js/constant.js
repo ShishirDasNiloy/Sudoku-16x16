5b356c5e728eb71ac50493ccedad3bfcf23ce8a3
Shishir <shishirdasniloy@gmail.com>
2022-01-23 02:22:30 +0600

    bug: accect wrong ans if complete al the empty cells

---
+++ assets/js/constant.js
@@ -0,0 +1,13 @@
+//declearing the values so for easy use in code
+const CONSTANT = {
+    UNASSIGNED: 0,
+    GRID_SIZE: 16,
+    BOX_SIZE: 4,
+    NUMBERS: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
+    LEVEL_NAME: [
+        'Easy',
+        'Medium',
+        'Hard',
+    ],
+    LEVEL: [35, 77, 120]
+}
